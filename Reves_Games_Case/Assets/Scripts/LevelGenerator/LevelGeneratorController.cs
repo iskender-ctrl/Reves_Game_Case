76c42b908ff066f347452e3117e4aa9cf6eacc70
iskender-ctrl <82265032+iskender-ctrl@users.noreply.github.com>
2022-11-24 02:25:48 +0300

    Script design. Instantiate randomly coind and obstacle.

--- Reves_Games_Case/Assets/Scripts/LevelGenerator/LevelGeneratorController.cs
+++ Reves_Games_Case/Assets/Scripts/LevelGenerator/LevelGeneratorController.cs
@@ -4,32 +4,65 @@ using UnityEngine;
 
 public class LevelGeneratorController : MonoBehaviour
 {
-    public GameObject[] runPartPrefabs, obstacles, platformObstaclesChild;
-    public GameObject startPrefabs, endPrefabs, coin;
+    public GameObject[] runPartPrefabs, obstacles;
+    [SerializeField] List<GameObject> newRunPartList = new List<GameObject>();
+    public GameObject coin, newRunPurt;
     Vector3 newPosition, lastPosition;
-    [SerializeField] List<GameObject> platformObjectsList = new List<GameObject>();
-    public int runPartCount, lastAddedObject;
-    public float objectDistance;
+    public int runPartCount, instantiateIndex, levelNumber;
+    int targetRunPartCountIndex;
+    public float runPartObjectDistance;
+    bool canContinue;
     void Start()
     {
         InstantiatePlatform();
+        InstantiateObstacleAndCoins();
     }
     void InstantiatePlatform()
     {
-        GameObject startObject = Instantiate(startPrefabs, Vector3.zero, startPrefabs.transform.rotation);
-        platformObjectsList.Add(startObject);
-
         for (int i = 0; i < runPartCount; i++)
         {
-            int random = Random.Range(0, runPartPrefabs.Length);
-            newPosition = platformObjectsList[i].transform.position;
-            GameObject newObject = Instantiate(runPartPrefabs[random].gameObject, new Vector3(0, 0, newPosition.z + objectDistance), runPartPrefabs[random].gameObject.transform.rotation);
-            platformObjectsList.Add(newObject);
-            lastAddedObject = i;
-        }
+            targetRunPartCountIndex++;
 
-        lastPosition = platformObjectsList[lastAddedObject].transform.position;
-        GameObject endObject = Instantiate(endPrefabs, new Vector3(0, 0, lastPosition.z + objectDistance + 10), endPrefabs.transform.rotation);
-        platformObjectsList.Add(endObject);
+            if (GameObject.Find("RunPart-Start(Clone)") == null)
+            {
+                instantiateIndex = 0;
+            }
+            else
+            {
+                int random = Random.Range(1, runPartPrefabs.Length - 1);
+                instantiateIndex = random;
+            }
+
+            if (targetRunPartCountIndex == runPartCount)
+            {
+                instantiateIndex = runPartPrefabs.Length - 1;
+            }
+
+            newPosition = InstantiateFunction(runPartPrefabs[instantiateIndex].gameObject, new Vector3(0, 0, lastPosition.z + runPartObjectDistance), runPartPrefabs[instantiateIndex].gameObject.transform.rotation);
+            lastPosition = newPosition;
+
+            if (newRunPurt.gameObject.tag == "RunPlatform")
+            {
+                newRunPartList.Add(newRunPurt);
+            }
+        }
+    }
+    void InstantiateObstacleAndCoins()
+    {
+        for (int i = 0; i < newRunPartList.Count; i++)
+        {
+            PlatformPart platformPart = newRunPartList[i].transform.GetComponent<PlatformPart>();
+            GameObject coinPoint = platformPart.coinPoint[Random.Range(0, platformPart.coinPoint.Length)];
+            InstantiateFunction(coin, coinPoint.transform.position, coin.transform.rotation);
+
+            GameObject point = platformPart.obstaclePoint[Random.Range(0, platformPart.obstaclePoint.Length)];
+            int randomObstacle = Random.Range(0, obstacles.Length);
+            InstantiateFunction(obstacles[randomObstacle], point.transform.position, Quaternion.Euler(obstacles[randomObstacle].transform.eulerAngles.x, -point.transform.localEulerAngles.y, obstacles[randomObstacle].transform.eulerAngles.z));
+        }
+    }
+    Vector3 InstantiateFunction(GameObject spawnObj, Vector3 position, Quaternion rotation)
+    {
+        newRunPurt = Instantiate(spawnObj, position, rotation);
+        return position;
     }
 }
